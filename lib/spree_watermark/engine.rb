d9afd98f3bfa3aa8a1d00f98e2ad268dbc423502
Ubuntu <ubuntu@ip-10-28-25-2.ec2.internal>
2012-10-31 18:47:48 +0000

    changes for spree 1.1

---
+++ lib/spree_watermark/engine.rb
@@ -0,0 +1,17 @@
+require "paperclip_processors/watermark.rb"
+
+module SpreeWatermark
+  class Engine < Rails::Engine
+  	engine_name 'spree_watermark'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.env.production? ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end
