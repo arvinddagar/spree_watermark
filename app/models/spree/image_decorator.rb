93e7f4582e0930a301a6e8be93bfea0c15d839af
SK <lunaticman@gmail.com>
2013-09-13 05:14:44 +0700

    rewrite default image_decorator

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,35 +1,27 @@
 Spree::Image.class_eval do
   has_attached_file(
-    :attachment,
-    :processors => [:thumbnail, :watermark],
-    :styles => {
-      :mini => {
-        :geometry => '48x48>',
-        :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/tiny.png",
-        :watermark_position => "SouthWest",
-        :format => :png,
+      :attachment,
+      :processors => [:thumbnail, :watermark],
+      :styles => {
+          :mini => '128x128>',
+
+          :small => '133x133>',
+
+          :product => {
+              :geometry => '398x398>',
+              :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_398x398.png",
+              :watermark_position => "Center",
+              :format => :png,
+          },
+          :large => {
+              :geometry => '600x600>',
+              :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_600x600.png",
+              :watermark_position => "Center",
+              :format => :png,
+          },
       },
-      :small => {
-        :geometry => '100x100>',
-        :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/small.png",
-        :watermark_position => "SouthWest",
-        :format => :png,
-      },
-      :product => {
-        :geometry => '240x240>',
-        :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/product.png",
-        :watermark_position => "SouthWest",
-        :format => :png,
-      },
-      :large => {
-        :geometry => '600x600>',
-        :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/large.png",
-        :watermark_position => "SouthWest",
-        :format => :png,
-      },
-    }, 
-    :default_style => :product,
-    :url => "/assets/products/:id/:style/:basename.:extension",
-    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
-    )
+      :default_style => :product,
+      :url => "/assets/products/:id/:style/:basename.:extension",
+      :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+  )
 end
