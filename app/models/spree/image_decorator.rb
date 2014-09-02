bbe327a65eaff6007f956ffd2814ac68f1589c21
arvinddagar <adarvinddagar@gmail.com>
2014-09-02 12:08:50 +0530

    Update image_decorator.rb

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -3,20 +3,34 @@ Spree::Image.class_eval do
       :attachment,
       :processors => [:thumbnail, :watermark],
       :styles => {
-          :mini => '128x128>',
-
-          :small => '133x133>',
-
+          :mini => {
+              :geometry => '48x48>',
+              :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_48x48.png",
+              :watermark_position => "Southwest",
+              :format => :png,
+          },  
+          :small => {
+              :geometry => '100x100>',
+              :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_100x100.png",
+              :watermark_position => "Southwest",
+              :format => :png,
+          },
+          :original => {
+              :geometry => '600x600>',
+              :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_600x600.png",
+              :watermark_position => "Southwest",
+              :format => :png,
+          },
           :product => {
               :geometry => '398x398>',
               :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_398x398.png",
-              :watermark_position => "Center",
+              :watermark_position => "Southwest",
               :format => :png,
           },
           :large => {
               :geometry => '600x600>',
               :watermark_path => "#{Rails.root.to_s}/public/images/watermarks/watermark_600x600.png",
-              :watermark_position => "Center",
+              :watermark_position => "Southwest",
               :format => :png,
           },
       },
